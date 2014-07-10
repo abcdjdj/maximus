bf8c11596dcec7a85e6d11bf8d36000b6c247bf1
abcdjdj <abcdjdj@gmail.com>
2014-07-10 22:55:55 +0530

    compile.sh : Make everything ready in 1 step

--- compile.sh
+++ compile.sh
@@ -1,7 +1,12 @@
+KERNEL_NAME=$1
 TARGET_DIR=~/stuff/compiler
 export CROSS_COMPILE=~/stuff/tc/arm-cortex_a15-linux-gnueabihf-linaro_4.9.1-2014.06/bin/arm-eabi-
 export ARCH=arm
 
 make tianchi_dual_defconfig
 make -j10
+rm $TARGET_DIR/zImage $TARGET_DIR/dt.img
 cp arch/arm/boot/zImage $TARGET_DIR
+cd $TARGET_DIR
+./generate_dt.sh
+. makeit.sh $KERNEL_NAME
